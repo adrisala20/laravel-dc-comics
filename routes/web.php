aa2963d239cc21de277838a01058738b7798b31c
adrisala20 <adriana_15_tuya@hotmail.com>
2024-05-27 17:32:02 +0200

    create folder comics in view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,43 +20,6 @@ Route::get('/', function () {
     $links_middle = config('footer_links_middle');
     return view('home', compact('comics','links_top','links_middle'));
 })->name('home');
+Route::resource('comics', ComicController::class);
 
-Route::get('/characters', function () {
-    return view('characters');
-})->name('characters');
 
-Route::get('/comics', function () {
-    return view('comics');
-})->name('comics');
-
-Route::get('/movies', function () {
-    return view('movies');
-})->name('movies');
-
-Route::get('/tv', function () {
-    return view('tv');
-})->name('tv');
-
-Route::get('/games', function () {
-    return view('games');
-})->name('games');
-
-Route::get('/collectibles', function () {
-    return view('collectibles');
-})->name('collectibles');
-
-Route::get('/videos', function () {
-    return view('videos');
-})->name('videos');
-
-Route::get('/fans', function () {
-    return view('fans');
-})->name('fans');
-
-Route::get('/new', function () {
-    return view('new');
-})->name('new');
-
-Route::get('/shops', function () {
-    return view('shops');
-})->name('shops');
